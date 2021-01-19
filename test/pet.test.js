dc5d5621cfe73eaff383bc31916e62f0e358410d
James <j.a.pratt@outlook.com>
2021-01-19 23:31:00 +0000

    pet named

--- test/pet.test.js
+++ test/pet.test.js
@@ -1,7 +1,12 @@
+const { it, expect } = require('@jest/globals');
 const Pet = require('../src/pet');
+const pet = new Pet('Iorek');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+      expect(new Pet('Iorek')).toBeInstanceOf(Object);
     });
+  });
+  it('returns the name of my pet', () => {
+      expect(pet.name).toBe("Iorek");
   });
