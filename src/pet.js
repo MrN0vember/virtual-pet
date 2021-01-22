caf2c96eacfb20c4a37970222fa814b55a3d030f
James <j.a.pratt@outlook.com>
2021-01-22 18:05:16 +0000

    aging increases hunger and reduces fitness added

--- src/pet.js
+++ src/pet.js
@@ -1,12 +1,22 @@
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
    
 };
 
-Pet.prototype.growUp = function () {
-    this.age +=1;
+Pet.prototype = {
+growUp() {
+    this.age++;
+},
+
+increasesHunger() {
+    this.hunger += 5;
+},
+
+decreaseFitness() {
+    this.fitness -= 3;
 }
-
-
+}
 module.exports = Pet;
--- test/pet.test.js
+++ test/pet.test.js
@@ -1,18 +1,37 @@
-const { it, expect } = require('@jest/globals');
+
 const Pet = require('../src/pet');
-const pet = new Pet('Iorek');
 
 describe('constructor', () => {
     it('returns an object', () => {
+        const pet = new Pet('Iorek');
       expect(new Pet('Iorek')).toBeInstanceOf(Object);
     });
   });
   it('returns the name of my pet', () => {
+    const pet = new Pet('Iorek');
       expect(pet.name).toBe("Iorek");
   });
   it('returns an initial age of 0', () => {
+    const pet = new Pet('Iorek');
       expect(pet.age).toBe(0);
   });
+
+
+
+describe('Hunger', () =>  {
+     it('returns an inital hunger value of 0', () => {
+        const pet = new Pet('Iorek');
+         expect(pet.hunger).toEqual(0);
+     });
+  });
+  
+  describe('Fitness', () => {
+      it('returns an initial fitness value of 10', () => {
+        const pet = new Pet('Iorek');
+         expect(pet.fitness).toEqual(10);
+      });
+  });
+  
   describe('growUp', () => {
       it('increases age by 1', () => {
           const pet = new Pet('Iorek');
@@ -20,3 +39,18 @@ describe('constructor', () => {
           expect(pet.age).toEqual(1);
       });
   });
+
+ describe('As pet ages hunger increase by 5 and fitness is reduced by 3', () => {
+  it('Pet hunger increases', () => {
+    const pet = new Pet('Iorek'); 
+      pet.growUp();
+      pet.increasesHunger ();
+      expect(pet.hunger).toBe(5);
+      });
+  it('Pet fitness decreases', () => {
+    const pet = new Pet('Iorek');
+      pet.growUp();
+      pet.decreaseFitness();
+      expect(pet.fitness).toBe(7);
+  });
+});
